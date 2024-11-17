9191d8af7ae5e71ecb65d1d4f91ddc25986edf29
virginiarm <92397676+virginiarm@users.noreply.github.com>
2024-11-17 13:49:48 -0500

    Move code to root directory and fix branch workflow

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const todoList = [
+    { id: 1, title:"Complete assigment"},
+    { id: 2, title: "Buy coffee supplies"},
+    { id: 3, title: "Water plants"},
+  ]
+  return (
+      <div>
+        <h1>Todo List</h1>
+        <ul>
+          {todoList.map((todo)=>(
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
+  )
+}
+
+export default App
