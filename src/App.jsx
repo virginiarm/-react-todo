27616e5cd04623f6cc3bc56df151cdeaad514035
virginiarm <92397676+virginiarm@users.noreply.github.com>
2024-12-17 17:06:50 -0500

    finisihing up shifting state and props handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import TodoList from './TodoList';
 
 
 function App(){
-  const[todoList,setTodoList]=useState(
+  const[todoList,setTodoList]=useState([]);(
     [{ id: 1, title:"Complete assigment"},
     { id: 2, title: "Buy coffee supplies"},
     { id: 3, title: "Water plants"},
   ]);
-  function addTodo(newTodo){
+  const addTodo= (newTodo)=> {
     setTodoList([...todoList,newTodo]);
   }
   return (
     <div>
       <h1>Todo App</h1>
-      <AddTodoForm onAddTodo={addTodo}/>
+      <AddTodoForm onAddTodo= {addTodo}/>
       <TodoList todoList={todoList}/>
 
     </div>
